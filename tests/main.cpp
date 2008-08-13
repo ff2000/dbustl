58e072b004e9e710bac3669d2d1b21a90c2d9904
fchevalier <fchevalier@d3dff9d2-17d8-11de-b208-e5a9466b8568>
2008-08-13 21:56:53 +0000

    First attempt at making return arguments work

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,16 +4,21 @@
 #include <dbus/dbus-glib.h>
 #include <dbus/dbus-glib-lowlevel.h>
 
+#include <iostream>
+
 int main()
 {
     
     dbustl::Connection * conn = dbustl::Connection::systemBus();
     dbus_connection_setup_with_g_main(conn->dbus(), NULL);
     try {
+        std::string foundService;
         dbustl::Proxy bluezManager(conn, "/org/bluez", "org.bluez");
         
         bluezManager.setInterface("org.bluez.Manager");
-        bluezManager.call("FindService", (const char *)"audio");
+        bluezManager.call("FindService", "audio", &foundService);
+        
+        std::cout << foundService << std::endl;
     }
     catch(...) {
       delete conn;
