ca4016f70def5f5a91dd1a92040788ac18786a62
fchevalier <fchevalier@d3dff9d2-17d8-11de-b208-e5a9466b8568>
2008-08-13 21:56:53 +0000

    First attempt at making return arguments work

--- trunk/src/Proxy.cpp
+++ trunk/src/Proxy.cpp
@@ -10,7 +10,6 @@ Proxy::Proxy(Connection* conn, const std::string& path, const std::string& desti
 {
 }
 
-//TODO : handle out of memory conditions
 DBusMessage * Proxy::newMethodCall(const std::string& methodName)
 {
     const char *dest = _destination.empty() ? NULL : _destination.c_str();
@@ -19,5 +18,26 @@ DBusMessage * Proxy::newMethodCall(const std::string& methodName)
     return dbus_message_new_method_call(dest, _path.c_str(), intf, methodName.c_str());
 }
 
+DBusMessage* Proxy::doBlockingCall(DBusMessage *method_call)
+{
+    DBusException error;
+    DBusMessage *reply;
+          
+    reply = dbus_connection_send_with_reply_and_block(_conn->dbus(), method_call, _timeout, error.dbus());
+    if(!reply) {
+        throw_or_set(error);
+    }
+    return reply;
+}
+
+bool Proxy::processCall(DBusMessage *method_call, DBusMessageIter *)
+{
+    if(_parsingInputArguments) {
+        //All arguments are consumed but we haven't called yet, do it now
+        MessageContainer reply(doBlockingCall(method_call));
+        return (reply.dbus() != NULL);
+    }
+    return true;
+}
 
 }
