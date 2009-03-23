e084e2acf27d370d275fde8a1a9d153adfd859d0
chefabien <chefabien@d3dff9d2-17d8-11de-b208-e5a9466b8568>
2009-03-23 21:59:21 +0000

    Recreated tag

---
+++ tags/0.2.0/examples/struct2.cpp
@@ -0,0 +1,48 @@
+/**
+ *  DBUSTL - DBus Template Library
+ *
+ *  Copyright (C) 2008  Fabien Chevalier <chefabien@gmail.com>
+ *  
+ *
+ *  This file is part of the DBus Template Library.
+ *
+ *  The DBus Template Library is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 2.1 of the License, or
+ *  (at your option) any later version.
+ *
+ *  DBus Template Library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with DBus Template Library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+ 
+#include <dbustl-1/dbustl>
+
+#include "struct1.h"
+#include <string>
+
+using namespace std;
+using namespace dbustl;
+ 
+int main()
+{
+    ServerProxy remoteObject(Connection::sessionBus(), 
+    "/PythonServerObject", "com.example.SampleService");
+    try {
+        ExampleStruct s1, s2;
+        s1.field1 = "String";
+        s1.field2 = 1.1;
+        remoteObject.call("test_ExampleStruct", s1, &s2); 
+        cout << s1.field1 << ", " << s1.field2 << endl;
+    }
+    catch(const DBusException& e) {
+        /* Dbus call failed: e.name() constains error cause */
+        cerr << e.what() << endl;
+    } 
+    return 0;
+}
---
+++ tags/0.2.0/tests/link-test-2.cpp
@@ -0,0 +1,172 @@
+#include <dbustl-1/Message>
+
+#include <dbustl-1/types/list>
+#include <dbustl-1/types/vector>
+#include <dbustl-1/types/set>
+#include <dbustl-1/types/map>
+
+void testMessage2(dbustl::Message& m)
+{
+	{
+		bool t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		char t;
+		//Note : deserialization does not exist for signed char due to lack of 
+		//adequate DBUS representation
+		m << t;
+	}
+	
+	{
+		unsigned char t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		signed char t;
+		//Note : deserialization does not exist for signed char due to lack of 
+		//adequate DBUS representation
+		m << t;
+	}
+	
+	{
+		short t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		unsigned short t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		signed short t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		int t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		unsigned int t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		signed int t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		long t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		unsigned long t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		signed long t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		long long t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		unsigned long long t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		signed long long t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		float t;
+		//Note : deserialization does not exist for float due to lack of 
+		//adequate DBUS representation
+		m << t;
+	}
+	
+	{
+		double t;
+		m >> t;
+		m << t;
+	}	
+	
+	{
+		const char * t = "";
+		//Note : deserialization does not exist;
+		m << t;
+	}
+	
+	{
+		//Note : deserialization does not exist;
+		m << "test";
+	}
+	
+	{
+		std::string t;
+		m >> t;
+		m << t;
+	}
+	
+	{
+		std::vector<std::string> v;
+		m >> v;
+		m << v;
+	}
+	{
+		std::list<std::string> v;
+		m >> v;
+		m << v;
+	}
+	{
+		std::set<std::string> v;
+		m >> v;
+		m << v;
+	}
+	{
+		std::multiset<std::string> v;
+		m >> v;
+		m << v;
+	}
+	{
+		std::map<double, std::string> v;
+		m >> v;
+		m << v;
+	}
+	{
+		std::map<std::string, std::list<double> > v;
+		m >> v;
+		m << v;
+	}
+	{
+		std::multimap<double, std::string> v;
+		m >> v;
+	}
+}
