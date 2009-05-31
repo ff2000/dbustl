fdc79bca636495d62b901c3beea8a1b8ea763dd5
chefabien <chefabien@d3dff9d2-17d8-11de-b208-e5a9466b8568>
2009-05-31 16:20:56 +0000

    Support for (de)serialisation of STL containers with user-provided memory allocators

--- trunk/tests/link-test-2.cpp
+++ trunk/tests/link-test-2.cpp
@@ -6,6 +6,64 @@
 #include <dbustl-1/types/set>
 #include <dbustl-1/types/map>
 
+#include <cstdlib>
+
+template<typename _Tp>
+    struct myLess : public std::binary_function<_Tp, _Tp, bool>
+    {
+        bool operator()(const _Tp& __x, const _Tp& __y) const
+        { 
+        	return __x < __y;
+        }
+    };
+    
+template <class T>
+class myAllocator
+{
+public:
+  typedef size_t    size_type;
+  typedef ptrdiff_t difference_type;
+  typedef T*        pointer;
+  typedef const T*  const_pointer;
+  typedef T&        reference;
+  typedef const T&  const_reference;
+  typedef T         value_type;
+
+  myAllocator() {}
+  myAllocator(const myAllocator&) {}
+
+
+
+  pointer   allocate(size_type n, const void * = 0) {
+              T* t = (T*) malloc(n * sizeof(T));
+              return t;
+            }
+  
+  void      deallocate(void* p, size_type) {
+              if (p) {
+                free(p);
+              } 
+            }
+
+  pointer           address(reference x) const { return &x; }
+  const_pointer     address(const_reference x) const { return &x; }
+  myAllocator<T>&  operator=(const myAllocator&) { return *this; }
+  void              construct(pointer p, const T& val) 
+                    { new ((T*) p) T(val); }
+  void              destroy(pointer p) { p->~T(); }
+
+  size_type         max_size() const { return size_t(-1); }
+
+  template <class U>
+  struct rebind { typedef myAllocator<U> other; };
+
+  template <class U>
+  myAllocator(const myAllocator<U>&) {}
+
+  template <class U>
+  myAllocator& operator=(const myAllocator<U>&) { return *this; }
+};
+
 void testMessage2(dbustl::Message& m)
 {
 	{
@@ -168,6 +226,37 @@ void testMessage2(dbustl::Message& m)
 	}
 	{
 		std::multimap<double, std::string> v;
+		m >> v;
+	}
+	/* Now test standard containers with non standard comparison operations
+	 * and allocators - we test with map to also instantiate the Signature */
+	{
+		std::map<int, std::list<std::string, myAllocator<std::string> > > v;
+		m >> v;
+		m << v;
+	}
+	{
+		std::map<int, std::vector<std::string, myAllocator<std::string> > > v;
+		m >> v;
+		m << v;
+	}
+	{
+		std::map<int, std::set<std::string, myLess<std::string>, myAllocator<std::string> > > v;
+		m >> v;
+		m << v;
+	}
+	{
+		std::map<int, std::multiset<std::string, myLess<std::string>, myAllocator<std::string> > > v;
+		m >> v;
+		m << v;
+	}
+	{
+		std::map<int, std::string, myLess<int>, myAllocator<std::pair<const int, std::string> > > v;
+		m >> v;
+		m << v;
+	}
+	{
+		std::multimap<int, std::string, myLess<int>, myAllocator<std::pair<const int, std::string> > >  v;
 		m >> v;
 	}
 }
