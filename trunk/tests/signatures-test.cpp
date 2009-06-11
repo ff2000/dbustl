04ed893a619edeec1ac4d6b523aebd74dd922958
chefabien <chefabien@d3dff9d2-17d8-11de-b208-e5a9466b8568>
2009-06-11 21:23:12 +0000

    Support for C++0x tuple data type

--- trunk/tests/signatures-test.cpp
+++ trunk/tests/signatures-test.cpp
@@ -11,6 +11,9 @@ int main()
 	assert(std::string("as") == dbustl::types::Signature<std::multiset<std::string> >());
 	assert((std::string("{ds}") == dbustl::types::Signature<std::pair<const double, std::string> >()));
 	assert((std::string("{sad}") == dbustl::types::Signature<std::pair<const std::string, std::list<double> > >()));
+#ifdef DBUSTL_CXX0X
+	assert((std::string("(iidii)") == dbustl::types::Signature<std::tuple<int, int, double, int, int> >()));
+#endif
 	std::cout << "Ok" << std::endl;
 	return 0;
 }
