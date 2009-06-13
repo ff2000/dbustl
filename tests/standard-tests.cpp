618149abd3b3a19f811708eee39f9cdef24dc12b
chefabien <chefabien@d3dff9d2-17d8-11de-b208-e5a9466b8568>
2009-06-13 16:31:19 +0000

    Merged signatures and standard tests

--- tests/standard-tests.cpp
+++ tests/standard-tests.cpp
@@ -859,6 +859,20 @@ int run_vt_tests()
 
 int main()
 {    
+	assert(std::string("as") == dbustl::types::Signature<std::vector<std::string> >());
+	assert(std::string("as") == dbustl::types::Signature<std::list<std::string> >());
+	assert(std::string("as") == dbustl::types::Signature<std::set<std::string> >());
+	assert(std::string("as") == dbustl::types::Signature<std::multiset<std::string> >());
+	assert(std::string("as") == dbustl::types::Signature<std::deque<std::string> >());
+	assert((std::string("{ds}") == dbustl::types::Signature<std::pair<const double, std::string> >()));
+	assert((std::string("{sad}") == dbustl::types::Signature<std::pair<const std::string, std::list<double> > >()));
+#ifdef DBUSTL_CXX0X
+	assert((std::string("as") == dbustl::types::Signature<std::array<std::string, 5> >()));
+	assert((std::string("(iidii)") == dbustl::types::Signature<std::tuple<int, int, double, int, int> >()));
+	assert(std::string("as") == dbustl::types::Signature<std::unordered_set<std::string> >());
+	assert(std::string("as") == dbustl::types::Signature<std::unordered_multiset<std::string> >());
+#endif
+
     int ret;
     ret = run_vt_tests();
     if(ret)
